dd5b8c225351689ac73c64be3bb5d3a7ec9ae00d
Jeffrey Rios Muñoz <jeffrey.rios@siesa.com>
2020-06-27 11:51:51 -0500

    Vistas parciales, Tag helper, incacion de nuevas vistas

--- HolaMundoMVC/Controllers/AsignaturaController.cs
+++ HolaMundoMVC/Controllers/AsignaturaController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using HolaMundoMVC.Models;
 using System;
 using CoreEscuela.Entidades;
+using System.Collections.Generic;
 
 namespace HolaMundoMVC.Controllers
 {
@@ -9,15 +10,44 @@ namespace HolaMundoMVC.Controllers
     {
         public IActionResult Index()
         {
-            var asignatura = new Asignatura{
-                UniqueId = Guid.NewGuid().ToString(),
-                Nombre = "Programacion"
+
+            return View(new Asignatura
+            {
+                Nombre = "Programacion",
+                UniqueId = Guid.NewGuid().ToString()
+            });
+        }
+        public IActionResult MultiAsignatura()
+        {
+            var listaAsignaturas = new List<Asignatura>() {
+                new Asignatura {
+                Nombre = "Matemáticas",
+                UniqueId = Guid.NewGuid ().ToString ()
+                },
+                new Asignatura {
+                Nombre = "Educación Física",
+                UniqueId = Guid.NewGuid ().ToString ()
+                },
+                new Asignatura {
+                Nombre = "Castellano",
+                UniqueId = Guid.NewGuid ().ToString ()
+                },
+                new Asignatura {
+                Nombre = "Ciencias Naturales",
+                UniqueId = Guid.NewGuid ().ToString ()
+                },
+                new Asignatura {
+                Nombre = "Programacion",
+                UniqueId = Guid.NewGuid ().ToString ()
+                }
             };
 
 
             ViewBag.CualquierCosa = "esto es cualquier cosa";
             ViewBag.Fecha = DateTime.Now;
-            return View(asignatura);
+
+
+            return View("MultiAsignatura", listaAsignaturas);
         }
     }
 }
