9275303b95f37b2a6f0bcbbebc3ab63e82fbe1af
Jeffrey Rios Muñoz <jeffrey.rios@siesa.com>
2020-07-02 22:15:07 -0500

    Implementacion para mapeo de datos en ORM

--- HolaMundoMVC/Models/Asignatura.cs
+++ HolaMundoMVC/Models/Asignatura.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 
 namespace HolaMundoMVC.Models
 {
-    public class Asignatura:ObjetoEscuelaBase
+    public class Asignatura : ObjetoEscuelaBase
     {
-        public string CursoId {get; set;}
-        public Curso Curso {get; set;}
+        public string CursoId { get; set; }
+        public Curso Curso { get; set; }
+        public List<Evaluación> Evaluaciones { get; set; }
     }
 }
