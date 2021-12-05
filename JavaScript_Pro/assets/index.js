9c7595c1342eacdbbd463cfe8bd44725c505043e
jeffrey2423 <riosjeff24@gmail.com>
2021-12-05 16:34:15 -0500

    nuevo curso

---
+++ JavaScript_Pro/assets/index.js
@@ -0,0 +1,19 @@
+import MediaPlayer from "./MediaPlayer.js";
+import AutoPlay from './plugins/AutoPlay.js'
+
+const video = document.querySelector("video");
+const button = document.querySelector("button");
+const muteButton = document.querySelector("#muteButton");
+
+const player = new MediaPlayer({
+    el: video,
+    // plugins: [new AutoPlay()]
+})
+
+button.onclick = () => {
+    player.togglePlay();
+}
+
+muteButton.onclick = () => {
+    player.toggleMute();
+}
